d2ab697a41565c359907d3e645d32c463cf10e92
Dimitris-Toulis <dimitritouli@gmail.com>
2025-01-06 19:04:37 +0200

    Add facing to MagicNullifier

--- src/main/java/net/toulis/magic/block/MagicNullifier.java
+++ src/main/java/net/toulis/magic/block/MagicNullifier.java
@@ -1,13 +1,19 @@
 package net.toulis.magic.block;
 
 import com.mojang.serialization.MapCodec;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.EnumProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import net.toulis.magic.ModBlockEntities;
 import org.jetbrains.annotations.Nullable;
@@ -15,6 +21,7 @@ import org.jetbrains.annotations.Nullable;
 public class MagicNullifier extends BlockWithEntity {
     public MagicNullifier(Settings settings) {
         super(settings);
+        setDefaultState(getDefaultState().with(FACING, Direction.NORTH));
     }
 
     @Override
@@ -28,6 +35,16 @@ public class MagicNullifier extends BlockWithEntity {
     @Override
     public BlockRenderType getRenderType(BlockState state) {
         return BlockRenderType.MODEL;
+    }
+
+    public static final EnumProperty<Direction> FACING = Properties.HORIZONTAL_FACING;
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
     }
 
     @Nullable
