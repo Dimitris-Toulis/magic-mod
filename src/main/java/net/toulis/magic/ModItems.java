50a9ba3515e95d2a143880c622550f33b3f11512
Dimitris-Toulis <dimitritouli@gmail.com>
2025-01-11 21:25:23 +0200

    Add concentrated magic dust

--- src/main/java/net/toulis/magic/ModItems.java
+++ src/main/java/net/toulis/magic/ModItems.java
@@ -1,7 +1,6 @@
 package net.toulis.magic;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.client.item.ModelPredicateProviderRegistry;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.Items;
@@ -14,6 +13,7 @@ import java.util.function.Function;
 
 public class ModItems {
     public static final Item MAGIC_DUST = register("magic_dust", Item::new, new Item.Settings());
+    public static final Item CONCENTRATED_MAGIC_DUST = register("concentrated_magic_dust", Item::new, new Item.Settings());
     public static final Item MAGIC_WAND_TIER_1 = register("magic_wand_tier_1", settings -> new MagicWand(1,settings), new Item.Settings().maxCount(1));
     public static final Item MAGIC_WAND_TIER_2 = register("magic_wand_tier_2", settings -> new MagicWand(2,settings), new Item.Settings().maxCount(1));
     public static final Item MAGIC_WAND_TIER_3 = register("magic_wand_tier_3", settings -> new MagicWand(3,settings), new Item.Settings().maxCount(1));
@@ -29,6 +29,7 @@ public class ModItems {
 
     public static void init(){
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(itemGroup -> itemGroup.add(MAGIC_DUST));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(itemGroup -> itemGroup.add(CONCENTRATED_MAGIC_DUST));
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(itemGroup -> itemGroup.add(MAGIC_WAND_TIER_1));
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(itemGroup -> itemGroup.add(MAGIC_WAND_TIER_2));
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(itemGroup -> itemGroup.add(MAGIC_WAND_TIER_3));
