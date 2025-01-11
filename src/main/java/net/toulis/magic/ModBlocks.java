bc93f81b0a3ec93789182607e63bb452c92746af
Dimitris-Toulis <dimitritouli@gmail.com>
2025-01-11 22:39:12 +0200

    Add wand editor block

--- src/main/java/net/toulis/magic/ModBlocks.java
+++ src/main/java/net/toulis/magic/ModBlocks.java
@@ -10,6 +10,7 @@ import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.toulis.magic.block.MagicNullifier;
+import net.toulis.magic.block.WandEditor;
 
 import java.util.function.Function;
 
@@ -17,6 +18,7 @@ public class ModBlocks {
     public static final Block MAGIC_ORE = register("magic_ore", Block::new, Block.Settings.create().strength(3f).requiresTool());
     public static final Block DEEPSLATE_MAGIC_ORE = register("deepslate_magic_ore", Block::new, Block.Settings.create().strength(5f).requiresTool());
     public static final Block MAGIC_NULLIFIER = register("magic_nullifier", MagicNullifier::new, Block.Settings.create().strength(1f));
+    public static final Block WAND_EDITOR = register("wand_editor", WandEditor::new, Block.Settings.create().strength(1f));
 
     private static Block register(String path, Function<AbstractBlock.Settings, Block> factory, AbstractBlock.Settings settings) {
         final Identifier identifier = Identifier.of(MagicMod.MOD_ID, path);
@@ -31,5 +33,6 @@ public class ModBlocks {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(itemGroup -> itemGroup.add(MAGIC_ORE));
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(itemGroup -> itemGroup.add(DEEPSLATE_MAGIC_ORE));
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(itemGroup -> itemGroup.add(MAGIC_NULLIFIER));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(itemGroup -> itemGroup.add(WAND_EDITOR));
     }
 }
